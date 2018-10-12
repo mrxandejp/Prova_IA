5c58f65a44ca828809eeb54c0355b7f7a168fde4
m3ryck <adriano.bsanto@eng.ci.ufpb.br>
2018-10-12 19:02:16 -0300

    Questao 2 finalizada

--- prova_IA_2.py
+++ prova_IA_2.py
@@ -147,7 +147,7 @@ scaler.fit(X_train)
 X_train = scaler.transform(X_train)  
 X_test = scaler.transform(X_test)  
 
-#REDE NEO GRAL
+#REDE NEURAL
 
 from sklearn.neural_network import MLPClassifier
 
