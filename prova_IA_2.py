52861f770115f00ff26c80343c51f57490d8322a
m3ryck <adriano.bsanto@eng.ci.ufpb.br>
2018-10-12 14:30:56 -0300

    Agreggate em curso

--- prova_IA_2.py
+++ prova_IA_2.py
@@ -10,6 +10,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-#IMPORTANDO DATABASE
 
+#IMPORTANDO DATABASE
 dataset = pd.read_csv('avila-tr.txt')
+
+#DETECÇÃO DE DADOS AUSENTES
+count_dados_ausentes = dataset.isnull().sum()
+
+#MAPA DE CALOR 'CORRELAÇÃO'
+correlacao=dataset.corr().abs().unstack().sort_values(kind='quicksort')
+sns.heatmap(dataset.corr())
+print(correlacao)
+
+df=pd.DataFrame(columns=)
+
+
+dataset.f6.combine(dataset.f10, lambda x1, x2: x1 if x1 < x2 else x2)
+
+
+
+
+
+
