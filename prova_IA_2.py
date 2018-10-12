06df9333cf9f083f9deb4ec6593d03c21eb1d96d
m3ryck <adriano.bsanto@eng.ci.ufpb.br>
2018-10-12 13:41:11 -0300

    Questao 2 atualizada

--- prova_IA_2.py
+++ prova_IA_2.py
@@ -6,3 +6,10 @@ Created on Fri Oct 12 13:11:35 2018
 @author: adriano
 """
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+#IMPORTANDO DATABASE
+
+dataset = pd.read_csv('avila-tr.txt')
