7b04c66edb28f217a06b18745fd8c5546be796e6
m3ryck <adriano.bsanto@eng.ci.ufpb.br>
2018-10-14 10:48:00 -0300

    Questao 4 atualizada

--- prova_IA_4.py
+++ prova_IA_4.py
@@ -10,6 +10,7 @@ import pandas as pd
 dataset = pd.read_csv("csv_result-Genes_Atividade_IA.csv")
 
 X = dataset.iloc[:,0:33] 
+
 X.drop(['id'],axis=1, inplace=True)
 
 
@@ -18,12 +19,20 @@ from sklearn.cluster import KMeans
 #n_clusters = 2, 5, 10, e 100
 #init = k-means++ / random / 1ª favorece a convergencia 2º randomico
 #max_iter = quantidade maxima de iteracoes
-kmeans = KMeans(n_clusters = 2, init = 'k-means++')
-#kmeans_5 = KMeans(n_clusters = 5, init = 'random')
-#kmeans_10 = KMeans(n_clusters = 10, init = 'random')
-#kmeans_100 = KMeans(n_clusters = 100, init = 'random')
 
-##################### k = 2 ###############################
+#kmeans = KMeans(n_clusters = 2, init = 'k-means++', max_iter=300)
+#k=2 apenas 1 instancia pertencente ao cluster 1
+
+#kmeans = KMeans(n_clusters = 5, init = 'random')
+#k=5 pouca variação , clusters 1 e 2 apenas com 1 instancia
+
+#kmeans = KMeans(n_clusters = 10, init = 'random')
+#k=10 ainda com pouca variação, dados super concentrados no cluster 0,
+#clusters 3 e 6, apenas com 1 instancia
+
+kmeans = KMeans(n_clusters = 100, init = 'random')
+#apresenta mais variações, aparentemente nenhum cluster dominando o outro
+
 #Ajustando
 kmeans.fit(X)
 
@@ -40,19 +49,44 @@ labels = kmeans.labels_
 clusters = kmeans.predict(X)
 
 #tabela com os valores//respectivos clusters
-
 table_finale = X
 table_finale['cluster'] = clusters
 
 
 
+###############################################################################
+#                                                                             #       
+#                      #Clustering Hierárquico#                               # 
+#                                                                             #           
+###############################################################################
 
 
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+import numpy as np
+
+np.set_printoptions(precision=5, suppress=True)
 
 
+Z = linkage(X, 'ward','euclidean')
 
 
+from scipy.cluster.hierarchy import cophenet
+from scipy.spatial.distance import pdist
 
+c, coph_dists = cophenet(Z, pdist(X))
+
+
+from scipy.cluster.hierarchy import fcluster
+max_d = 50
+
+#criação de  1300 clusters
+clusters_H = fcluster(Z, max_d, criterion='distance')
+clusters_H
+ 
+
+table_finale_H = X
+table_finale_H['cluster'] = clusters_H
 
 
 
